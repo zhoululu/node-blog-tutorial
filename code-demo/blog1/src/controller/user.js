6b934e29a1d224e110adb37d36b8e14c60a1c24d
周陆陆 <zhoululu@aegis-data.cn>
2022-12-08 15:43:20 +0800

    防止sql注入

--- code-demo/blog1/src/controller/user.js
+++ code-demo/blog1/src/controller/user.js
@@ -1,6 +1,11 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
+
 const login = (username, password) => {
-    const sql = `select  username, realname from users where username='${username}' and password='${password}';`
+    // 防止sql注入
+    username = escape(username)
+    password = escape(password)
+    // 使用escape函数后，${username}和${password}去除之前的前后单引号(')
+    const sql = `select  username, realname from users where username=${username} and password=${password};`
     return exec(sql).then(data => {
         return data[0] || {}
     })
