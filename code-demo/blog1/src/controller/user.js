7783902f2adb6eeba300a8c87ce04ed02084332c
周陆陆 <zhoululu@aegis-data.cn>
2022-12-09 15:44:09 +0800

    对密码进行加密

--- code-demo/blog1/src/controller/user.js
+++ code-demo/blog1/src/controller/user.js
@@ -1,8 +1,10 @@
 const { exec, escape } = require('../db/mysql')
-
+const { genPassword } = require('../utils/cryp')
 const login = (username, password) => {
     // 防止sql注入
     username = escape(username)
+    // 对密码加密
+    password = genPassword(password)
     password = escape(password)
     // 使用escape函数后，${username}和${password}去除之前的前后单引号(')
     const sql = `select  username, realname from users where username=${username} and password=${password};`
