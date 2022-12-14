c3c0de34f5af2d5478744e79c7bf7b11c3038d35
周陆陆 <zhoululu@aegis-data.cn>
2022-12-14 16:38:06 +0800

    koa2 test

---
+++ code-demo/koa-test/index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  console.log('1')
+   next();
+  console.log('2')
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  console.log('3')
+  const start = Date.now();
+  next();
+  console.log('4')
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+  console.log('5')
+  ctx.body = 'Hello World';
+  console.log('6')
+});
+
+app.listen(3000);
